d288f6dd2df9d13e1114a5156caba501e2cec3b2
Mansur Ziiatdinov <gltronred@gmail.com>
2020-07-23 17:49:39 +0300

    Parse config from sheets

--- src/TFB/Sheets.hs
+++ src/TFB/Sheets.hs
@@ -1,10 +1,15 @@
 -- | Interacting with Google sheets
 
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module TFB.Sheets where
 
-import TFB.Types (FieldType(..), FieldVal(..), Config(..), MsgItem(..))
+import TFB.Types ( FieldType(..)
+                 , FieldDef(..)
+                 , FieldVal(..)
+                 , FormConfig(..)
+                 , MsgItem(..))
 import TFB.Env (TFB,Env(Env,envConfig,envQueue))
 
 import Control.Concurrent.STM
@@ -21,7 +26,7 @@ import qualified Data.Text as T
 import qualified Data.Text.Lazy as LT
 import qualified Data.Text.Lazy.Encoding as ET
 import Network.Google.Sheets
-import Network.Google hiding (Env)
+import Network.Google hiding (Env, Error)
 import System.IO (stdout)
 
 sheetWorker :: Env TFB -> IO ()
@@ -31,11 +36,44 @@ sheetWorker Env{ envConfig=cfg, envQueue=mq } = do
     msg <- atomically $ readTBQueue mq
     print msg
     case msg of
-      MsgInfo ans -> appendRow cfg sheetId ans
+      MsgInfo form ans -> appendRow form ans
 
-appendRow :: Config -> Text -> Map Text FieldVal -> IO Text
-appendRow cfg sheetId ans = do
-  let fields = [] -- getFieldNames cfg
+parseConfig :: Text -> Text -> IO [FieldDef]
+parseConfig docId sheet = do
+  let range = "A:D"
+      req = spreadsheetsValuesGet docId $ sheet <> "!" <> range
+  lgr <- newLogger Debug stdout
+  env <- newEnv
+         <&> (envLogger .~ lgr)
+         . (envScopes .~ spreadsheetsScope)
+  resp <- runResourceT . runGoogle env $ send req
+  let res2log :: Result FieldDef -> IO (Maybe FieldDef)
+      res2log (Success x) = pure $ Just x
+      res2log (Error e) = putStrLn ("Can't parse field: " <> e) >> pure Nothing
+  fmap catMaybes $ forM (resp ^. vrValues) $ \case
+    [nameV, descV, fieldV, paramV] -> res2log $ FieldDef
+      <$> fromJSON nameV
+      <*> fromJSON descV
+      <*> case fromJSON fieldV :: Result Text of
+            Success "int" -> pure FieldInt
+            Success "num" -> pure FieldNum
+            Success "text"-> pure FieldText
+            Success "enum" -> FieldEnum . parseEnumOptions <$> fromJSON paramV
+            Success "location" -> FieldLocation <$> fromJSON paramV
+            Success "time" -> pure FieldTime
+            Success "source" -> pure FieldSource
+            Success "welcome" -> pure FieldWelcome
+            Success "thanks" -> pure FieldThanks
+            Success ty -> Error $ "Wrong type: " <> T.unpack ty
+            Error e -> Error e
+    row -> putStrLn ("Can't parse " <> show row) >> pure Nothing
+
+parseEnumOptions :: Text -> [[Text]]
+parseEnumOptions = map (T.splitOn ";") . T.splitOn ";;"
+
+appendRow :: FormConfig -> Map Text FieldVal -> IO Text
+appendRow FormConfig{ cfgResultSheet=sheetId, cfgFields=fs } ans = do
+  let fields = map fdName fs
       orderedAns = map (ans M.!) fields
   appendGS sheetId "raw" orderedAns
 
@@ -66,5 +104,8 @@ toJV :: FieldVal -> IO [Value]
 toJV (ValInt n) = pure $ pure $ toJSON n
 toJV (ValNum d) = pure $ pure $ toJSON d
 toJV (ValText t) = pure $ pure $ toJSON t
+toJV (ValEnum t) = pure $ pure $ toJSON t
+toJV (ValLocation (lat,lon)) = pure [toJSON lat, toJSON lon]
 toJV (ValTime ts) = pure $ pure $ toJSON ts
 toJV (ValUser u) = pure $ pure $ toJSON u
+toJV ValVoid = pure []
