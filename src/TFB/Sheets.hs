c4edee1a3c8eafdac49b57e6ab45cd365f73e7c0
Mansur Ziiatdinov <gltronred@gmail.com>
2020-08-04 08:56:51 +0300

    Add copyright notice in sources

--- src/TFB/Sheets.hs
+++ src/TFB/Sheets.hs
@@ -1,3 +1,23 @@
+{------------------------------------------------------------------------------
+
+    tg-form - Telegram bot for creating forms from Google Sheets
+    Copyright (C) 2020  Mansur Ziiatdinov
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+------------------------------------------------------------------------------}
+
 -- | Interacting with Google sheets
 
 {-# LANGUAGE LambdaCase #-}
